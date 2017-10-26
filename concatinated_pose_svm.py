f0c145b7c8a3e2bf9171ff92593f686e5feab9eb
ya4615 <ya4615@naver.com>
2017-10-27 02:50:53 +0900

    bug 수정

--- concatinated_pose_svm.py
+++ concatinated_pose_svm.py
@@ -72,34 +72,22 @@ class DataLoader:
 
         if file_name in os.listdir(self._save_dir_path):
             f = open(save_file_path, 'a')
-            iter = 1
-            for dat in _list_data:
-                f.write(dat)
-
-                if len(data) == iter:
-                    f.write("\n")
-                    continue
-
-                f.write(",")
-                iter += 1
-
-            f.close()
 
         else:
             f = open(save_file_path, 'w')
 
-            iter = 1
-            for dat in _list_data:
-                f.write(dat)
+        iter = 1
+        for dat in _list_data:
+            f.write(dat)
 
-                if len(data) == iter:
-                    f.write("\n")
-                    continue
+            if len(_list_data) == iter:
+                f.write("\n")
+                continue
 
-                f.write(",")
-                iter += 1
+            f.write(",")
+            iter += 1
 
-            f.close()
+        f.close()
 
     def preprocess_data_(self, _ground_truth="macro", _nomalize=True, _scaling=True):
         for file_number in self._file_num_list:
@@ -125,7 +113,7 @@ class DataLoader:
                         label = 0
                         if int(object.find('Type').text) == 111:
                             if _ground_truth == "macro":
-                                if check_macro_file(file, int(attr['frameNum'])):
+                                if check_macro_file(file_number, int(attr['frameNum'])):
                                     label = 1
                             """
                             else:
@@ -139,16 +127,17 @@ class DataLoader:
 
     def load_data_(self, _interval_size, _step_size, _posi_threshold):
 
+        action_data = []
         for file_name in os.listdir(self._save_dir_path):
 
             if int(file_name.split(".")[0]) not in self._file_num_list:
                 continue
 
-            _data_dir_path = self._save_dir_path + file_name
+            _data_dir_path = self._save_dir_path + "\\" + file_name
             f = open(_data_dir_path, 'r')
 
-
             data = {}
+
             for lines in f.readlines():
                 split_line = lines.split(',')
 
@@ -162,10 +151,41 @@ class DataLoader:
                 data[int(split_line[0])][int(split_line[2])].append(int(split_line[-1]))
             f.close()
 
-        action_data = self.packaging_load_data_(data, _interval_size, _step_size, _posi_threshold)
+            for person_id in data.keys():
+                frame_key = data[person_id].keys()
+                frame_key.sort()
+                if len(frame_key) < _interval_size:
+                    continue
+                start = 0
+                end = _interval_size
+                while 1:
+                    if end >= len(frame_key):
+                        break
+
+                    if frame_key[end] != frame_key[start] + _interval_size:
+                        break
+
+                    label_check = 0
+                    action_data.append([])
+                    for i in frame_key[start:end]:
+                        for j in range(36):
+                            action_data[-1].append(data[person_id][i][j])
+
+                        if data[person_id][i][-1] == 1:
+                            label_check += 1
+
+                    if label_check > _posi_threshold:
+                        action_data[-1].append(1)
+
+                    else:
+                        action_data[-1].append(0)
+
+                    start += _step_size
+                    end += _step_size
 
         return action_data
 
+    """
     @staticmethod
     def packaging_load_data_(_read_data, _interval_size, _step_size, _posi_threshold):
 
@@ -203,6 +223,7 @@ class DataLoader:
                 end += _step_size
 
         return action_data
+    """
 
 
 def check_macro_file(_file_num, _frame_num):
@@ -220,168 +241,6 @@ def check_macro_file(_file_num, _frame_num):
 
     f.close()
     return result
-
-"""
-def read_pose_(filename):
-    f = open(filename, 'r')
-    js = json.loads(f.read())
-    f.close()
-
-    return js
-
-
-
-# TODO: using verb xml file
-def check_verb_file(_file_num, _frame_num):
-    verb_file_path = ""
-
-
-
-def make_preprocess_data(_xml_path, _json_path, _save_path, _using_macro=False):
-    for file in files:
-        xml_file_path = _xml_path + "\\%03d.xml" % file
-
-        tree = parse(xml_file_path)
-        root = tree.getroot().find('Objects')
-        for object in root.findall('Object'):
-            if not int(object.find('Type').text) in [1, 111]:
-                continue
-
-            tracks = object.find('Tracks')
-            for track in tracks.findall('Track'):
-                attr = track.attrib
-                json_file_path = _json_path + "\\%03d\\%03d_%012d_keypoints.json" % (file, file, int(attr['frameNum']))
-
-                people = read_pose_(json_file_path)['people']
-                for person in people:
-                    key_point = person['pose_keypoints']
-
-                    if not (int(attr['X']) <= key_point[3] <= int(attr['X']) + int(attr['W']) and \
-                                            int(attr['Y']) <= key_point[4] <= int(attr['Y']) + int(attr['H'])):
-                        continue
-
-                    label = 0
-                    if int(object.find('Type').text) == 111:
-                        if _using_macro:
-                            if check_macro_file(file, int(attr['frameNum'])):
-                                label = 1
-                        
-                        else:
-                            if check_verb_file(file, int(attr['frameNum'].text)):
-                                label = 1
-                        
-
-                    data = []
-                    data.append(object.find('ID').text)
-                    data.append(object.find('Type').text)
-                    data.append(attr['frameNum'])
-
-                    norm_key_point = normalize_pose_(key_point)
-                    scaling_key_point = scaling_data_(norm_key_point)
-                    for i in range(18):
-                        data.append(str(scaling_key_point[i * 3]))
-                        data.append(str(scaling_key_point[i * 3 + 1]))
-                    data.append(str(label))
-
-                    file_name = "%06d.txt" % file
-                    if file_name in os.listdir(_save_path):
-                        txt_path = _save_path + "\\%s" % file_name
-                        f = open(txt_path, 'a')
-
-                        iter = 1
-                        for dat in data:
-                            f.write(dat)
-
-                            if len(data) == iter:
-                                f.write("\n")
-                                continue
-
-                            f.write(",")
-                            iter += 1
-                        f.close()
-
-                    else:
-                        txt_path = _save_path + "\\%s" % file_name
-                        f = open(txt_path, 'w')
-
-                        iter = 1
-                        for dat in data:
-                            f.write(dat)
-
-                            if len(data) == iter:
-                                f.write("\n")
-                                continue
-
-                            f.write(",")
-                            iter += 1
-                        f.close()
-
-
-def make_macro_file_to_dict():
-    macro_path = "C:\Users\JM\Desktop\Data\ETRIrelated\pose classification\class1macro.txt"
-    f = open(macro_path, 'r')
-
-    littering_info = {}
-    for lines in f.readlines():
-        split_line = lines.split(" ")
-        littering_info[int(split_line[0])] = [int(split_line[1]), int(split_line[2])]
-
-    f.close()
-    return littering_info
-
-
-def data_loader(_data_dir_path, _interval_size, _step_size, _posi_threshold):
-    f = open(_data_dir_path, 'r')
-
-    action_data = []
-    data = {}
-    for lines in f.readlines():
-        split_line = lines.split(',')
-
-        if not int(split_line[0]) in data.keys():
-            data[int(split_line[0])] = {}
-
-        data[int(split_line[0])][int(split_line[2])] = []
-        split_data = split_line[3:39]
-        for i in range(len(split_data)):
-            data[int(split_line[0])][int(split_line[2])].append(float(split_data[i]))
-        data[int(split_line[0])][int(split_line[2])].append(int(split_line[-1]))
-    f.close()
-
-    for person_id in data.keys():
-        frame_key = data[person_id].keys()
-        frame_key.sort()
-        if len(frame_key) < _interval_size:
-            continue
-        start = 0
-        end = _interval_size
-        while 1:
-            if end >= len(frame_key):
-                break
-
-            if frame_key[end] != frame_key[start] + _interval_size:
-                break
-
-            label_check = 0
-            action_data.append([])
-            for i in frame_key[start:end]:
-                for j in range(36):
-                    action_data[-1].append(data[person_id][i][j])
-
-                if data[person_id][i][-1] == 1:
-                    label_check += 1
-
-            if label_check > _posi_threshold:
-                action_data[-1].append(1)
-
-            else:
-                action_data[-1].append(0)
-
-            start += _step_size
-            end += _step_size
-
-    return action_data
-"""
 
 
 ########################################################################
@@ -455,6 +314,7 @@ if __name__ == '__main__':
     y = np.asarray(y)
     precision = 0
     recall = 0
+    print set(y)
     for train_index, test_index in skf.split(X, y):
 
         X_train, X_test = X[train_index], X[test_index]
